edfef7c90130e56e9ca4f79198140856e132d74e
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-08 13:31:03 +0200

    Add objects of class Person

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,5 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Person person1 = new Person("Nick", 25, "Logistic manager");
+        Person person2 = new Person("Marina", 60, "Teacher");
+        Person person3 = new Person("Andrey", 50, "Entrepreneur");
+        Person person4 = new Person("Alexander", 25, "Student");
     }
 }
