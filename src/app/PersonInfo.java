f114e7867b39edb205e95300043f64f23545d7a1
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-08 13:37:24 +0200

    Add new method getInfo in class PersonInfo

--- src/app/PersonInfo.java
+++ src/app/PersonInfo.java
@@ -1,4 +1,9 @@
 package app;
 
 public class PersonInfo {
+
+    public static void getInfo(String name, int age, String profession) {
+        System.out.print("Name: " + name + ", Age: " + age +
+                ", Profession: " + profession + "\n");
+    }
 }
