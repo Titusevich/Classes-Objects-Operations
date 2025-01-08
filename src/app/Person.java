d6010902925b79bd89d1e63e03a6f5f208fece35
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-08 13:28:10 +0200

    Add constructor of class Person

--- src/app/Person.java
+++ src/app/Person.java
@@ -4,4 +4,10 @@ public class Person {
     public  String name;
     public  int age;
     public  String profession;
+
+    public Person (String name,int age,String profession){
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
 }
