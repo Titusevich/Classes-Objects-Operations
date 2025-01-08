d7bd871623b34795a39deed9ce8d2292084fe2ad
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-08 13:40:33 +0200

    Add objects information

--- src/app/Main.java
+++ src/app/Main.java
@@ -9,5 +9,21 @@ public class Main {
         Person person2 = new Person("Marina", 60, "Teacher");
         Person person3 = new Person("Andrey", 50, "Entrepreneur");
         Person person4 = new Person("Alexander", 25, "Student");
+
+        count++;
+        System.out.println();
+        System.out.print(count + ". ");
+        PersonInfo.getInfo(person1.name,person1.age,person1.profession);
+
+        System.out.print("   ");
+        PersonInfo.getInfo(person2.name,person2.age,person2.profession);
+
+        System.out.print("   ");
+        PersonInfo.getInfo(person3.name,person3.age,person3.profession);
+
+        count++;
+        System.out.println();
+        System.out.print(count + ". ");
+        PersonInfo.getInfo(person4.name,person4.age,person4.profession);
     }
 }
